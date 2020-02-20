d98744417bbe031852805d7e09213b0773b99d0f
smootok <smootok@gmail.com>
2020-02-20 23:17:39 +0100

    Add script that import/delete items from output file to mongodb

---
+++ db-import-delete.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+
+const uri = 'mongodb://localhost:27017/edofus'
+
+const itemSchema = new mongoose.Schema({}, { strict: false })
+const Item = mongoose.model('Item', itemSchema)
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('DB connection successful!'))
+
+const importItems = async fileName => {
+  try {
+    const items = JSON.parse(
+      fs.readFileSync(`${__dirname}/output/${fileName}`, 'utf-8')
+    )
+    await Item.create(items)
+    console.log('Data successfully loaded!')
+  } catch (err) {
+    console.log(err)
+  }
+  process.exit()
+}
+
+const deleteItems = async () => {
+  try {
+    await Item.deleteMany()
+    console.log('Data successfully deleted!')
+  } catch (err) {
+    console.log(err)
+  }
+  process.exit()
+}
+
+if (process.argv[2] === '--import' && process.argv[3]) {
+  const fileName = process.argv[3]
+  importItems(fileName)
+} else if (process.argv[2] === '--delete') {
+  deleteItems()
+} else {
+  console.log('Command Not Found')
+  process.exit()
+}
